46f3969a609ef683256a370b96528f0546f623d8
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-26 01:22:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! THis is so awesome I am so happy that this is what we are seeing"
+  get '/' do
+    <h>Welcome to your app!!!! THis is so awesome I am so happy that this is what we are seeing</h>
   end
 
 end
